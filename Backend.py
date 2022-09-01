da5a8288c8ca602e06bd7e4f54f1f5ea0349a7f4
Hessum <hessumg@gmail.com>
2022-09-01 14:29:10 +0430

    Converted the program to an OOP one!

--- Backend.py
+++ Backend.py
@@ -4,44 +4,51 @@ import pandas as pd
 import datetime
 import jdatetime
 
-def Insert_Auto_Increment(Table, List):
+class SQL_Control:
 
-    query = 'SELECT * FROM %s;' %Table
-    unit_of_measure_df = pd.read_sql(query, cnx)
-    id = len(unit_of_measure_df) + 1
-    List.insert(0, id)
+    def __init__(self, user, password, host, database):
+        self.cnx = cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
+        self.cursor = cnx.cursor()
+        print('Successfully connected to the MySQL database')
 
-    format_strings = ','.join(['%s'] * len(List))
-    try: 
-        cursor.execute('INSERT INTO %s VALUES (%s)' % (Table, format_strings), tuple(List))
-        cnx.commit()
-        return 'Successfully added to the table %s' %Table
+    def Insert_Auto_Increment(self, Table, List):
 
-    except mysql.connector.Error as err:
-        if err.errno == 1062: 
-            return 'This is a redundant information'
+        query = 'SELECT * FROM %s;' %Table
+        unit_of_measure_df = pd.read_sql(query, self.cnx)
+        id = len(unit_of_measure_df) + 1
+        List.insert(0, id)
 
-def Edit_Row(Table, conidtion_column, condition, target_column, variable):
- 
-    try:
-        cursor.execute('UPDATE %s SET %s = \'%s\' WHERE %s = \'%s\';' % (Table, target_column, variable, conidtion_column, condition))
-        cnx.commit()
-        return 'Successfully edited the row where %s = %s' %(conidtion_column, condition)
+        format_strings = ','.join(['%s'] * len(List))
+        try: 
+            self.cursor.execute('INSERT INTO %s VALUES (%s)' % (Table, format_strings), tuple(List))
+            self.cnx.commit()
+            return 'Successfully added to the table %s' %Table
 
-    except mysql.connector.Error as err:
-        if err.errno == 1062: 
-            return 'This is a redundant information'
+        except mysql.connector.Error as err:
+            if err.errno == 1062: 
+                return 'This is a redundant information'
 
-def Delete_Row(Table, conidtion_column, condition):
+    def Edit_Row(self, Table, conidtion_column, condition, target_column, variable):
     
-    cursor.execute('DELETE FROM %s WHERE %s = \'%s\';' % (Table, conidtion_column, condition))
-    cnx.commit()
-    return 'Successfully deleted the row where %s = %s' %(conidtion_column, condition)
+        try:
+            self.cursor.execute('UPDATE %s SET %s = \'%s\' WHERE %s = \'%s\';' % (Table, target_column, variable, conidtion_column, condition))
+            self.cnx.commit()
+            return 'Successfully edited the row where %s = %s' %(conidtion_column, condition)
+
+        except mysql.connector.Error as err:
+            if err.errno == 1062: 
+                return 'This is a redundant information'
+
+    def Delete_Row(self, Table, conidtion_column, condition):
+    
+        self.cursor.execute('DELETE FROM %s WHERE %s = \'%s\';' % (Table, conidtion_column, condition))
+        self.cnx.commit()
+        return 'Successfully deleted the row where %s = %s' %(conidtion_column, condition)
+
 
 ############# Main ##################
 
-cnx = mysql.connector.connect(user='root', password='harchi', host='127.0.0.1', database='pos')
-cursor = cnx.cursor()
+SQL = SQL_Control('root', 'harchi', '127.0.0.1', 'pos')
 
 Table = 'unit_of_measure'
 List = ['مثقال']
@@ -49,6 +56,6 @@ conidtion_column = 'id'
 condition = '4'
 target_column = 'name'
 variable = 'بسته تک عددی'
-#print(Insert_Auto_Increment(Table, List))
-#print(Edit_Row(Table, conidtion_column, condition, target_column, variable))
-#print(Delete_Row(Table, conidtion_column, condition))
+#print(SQL.Insert_Auto_Increment(Table, List))
+#print(SQL.Edit_Row(Table, conidtion_column, condition, target_column, variable))
+#print(SQL.Delete_Row(Table, conidtion_column, condition))
