ba644db855ef984e9253a6f9f639cced2e61a792
Hessum <hessumg@gmail.com>
2022-09-01 12:47:12 +0430

    Edit sql table row added.

--- Backend.py
+++ Backend.py
@@ -14,12 +14,23 @@ def Insert_Auto_Increment(Table, List):
     format_strings = ','.join(['%s'] * len(List))
     try: 
         cursor.execute('INSERT INTO %s VALUES (%s)' % (Table, format_strings), tuple(List))
+        cnx.commit()
         return 'Successfully added to the table %s' %Table
+
     except mysql.connector.Error as err:
         if err.errno == 1062: 
             return 'This is a redundant information'
 
-    cnx.commit()
+def Edit_Row(Table, conidtion_column, condition, target_column, variable):
+ 
+    try:
+        cursor.execute('UPDATE %s SET %s = \'%s\' WHERE %s = \'%s\';' % (Table, target_column, variable, conidtion_column, condition))
+        cnx.commit()
+        return 'Successfully edited the row where %s = %s' %(conidtion_column, condition)
+
+    except mysql.connector.Error as err:
+        if err.errno == 1062: 
+            return 'This is a redundant information'
 
 ############# Main ##################
 
@@ -28,4 +39,9 @@ cursor = cnx.cursor()
 
 Table = 'unit_of_measure'
 List = ['مثقال']
-print(Insert_Auto_Increment(Table, List))
+conidtion_column = 'id'
+condition = '4'
+target_column = 'name'
+variable = 'بسته تک عددی'
+#print(Insert_Auto_Increment(Table, List))
+#print(Edit_Row(Table, conidtion_column, condition, target_column, variable))
